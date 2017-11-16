20e6482511c9b087720a41f1e9e712148e957ba0
Neous1 <Neous1@users.noreply.github.com>
2017-11-16 11:59:23 -0500

    swapping  values without variables

--- swapping.c
+++ swapping.c
@@ -1,16 +1,33 @@
 #include<stdio.h>
 
 int main(void){
-    int a, b, temp;
+    // int a, b, temp;
+
+    // printf("Enter two numbers \n");
+    // // scanf("value of a is %d and value of b is %d", &a, &b);
+    // scanf(" %d %d", &a, &b);
+
+    // temp = a; 
+    // a = b;
+    // b = temp;
+    
+    // printf("Swapped values are %d and %d\n\n", a, b);
+
+
+    /**
+     * how to swap the values of 2 variable with just 2 variables
+     */
+
+    int c, d ;
 
     printf("Enter two numbers \n");
     // scanf("value of a is %d and value of b is %d", &a, &b);
-    scanf(" %d %d", &a, &b);
+    scanf(" %d %d", &c, &d);
 
-    temp = a; 
-    a = b;
-    b = temp;
+    c = c + d; // clear the value at c
+    d = c - d; // put value of d in c
+    c = c - d; // put value c in d
     
-    printf("Swapped values are %d and %d\n\n", a, b);
+    printf("Swapped values are %d and %d\n\n", c, d);
 
 }
